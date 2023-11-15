7ec7d61fbc27c395cdf478fc13626fce49071de9
LucasPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2023-11-15 16:24:59 +0100

    (MainMenu) Put all the scripts in one
    It's all in the button play script

--- Scenes/Menu/Scripts/ButtonPlay.gd
+++ Scenes/Menu/Scripts/ButtonPlay.gd
@@ -1,20 +1,41 @@
 extends Button
 
-var isButtonGoingDown: bool = false
+var isGoingDown : bool = false
+var isGoingUp : bool = false
+var isGoingRight : bool = false
+var isGoingLeft : bool = false
 
 func _process(delta):
-	if isButtonGoingDown:
+	if isGoingDown:
 		position.y += 400 * delta
 		if position.y > 800:
-			isButtonGoingDown = false
+			isGoingDown = false
 			pass
 	
-	if $ButtonBack.isGoingUp:
+	if isGoingUp:
 		position.y -= 400 * delta
 		if position.y < 250:
-			$ButtonBack.isGoingUp = false
+			isGoingUp = false
 	
+	if isGoingLeft:
+		position.x -= 600 * delta
+		if position.y < 500: #A changer la value
+			isGoingLeft = false
+		
+	if isGoingRight:
+		position.x += 600 * delta
+		if position.x > 1575:
+			isGoingRight = false
 
 # When the button is pressed, other buttons will display
 func _on_pressed():
-	isButtonGoingDown = true
+	isGoingDown = true
+
+func _on_button_back_pressed():
+	isGoingUp = true
+
+func _on_button_1p_pressed():
+	isGoingRight = true
+
+func _on_button_back_1p_pressed():
+	isGoingLeft = true
