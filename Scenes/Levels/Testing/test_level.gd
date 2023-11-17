6c051a58fa1fb507af5b1f6ff943251b340e184a
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2023-11-17 11:28:08 +0100

    edit

--- Scenes/Levels/Testing/test_level.gd
+++ Scenes/Levels/Testing/test_level.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
-
-func _on_player_dropper_piece_detected(chosePiece):
-	var piece = chosePiece.instantiate() as RigidBody2D
-	piece.position = $PlayerDropper.global_position
-	$Pieces.add_child(piece)
+func _on_player_dropper_piece_detected(chosePiece: String):
+	print(chosePiece)
+	var piece_scene = load(chosePiece)   
+	if piece_scene and piece_scene is PackedScene:
+		var piece_instance = piece_scene.instance()      
+		if piece_instance and piece_instance is RigidBody2D:
+			var piece = piece_instance as RigidBody2D
+			piece.position = $PlayerDropper.global_position
+			$Pieces.add_child(piece)
+		else:
+			print("Erreur : La scène chargée n'est pas une instance de RigidBody2D.")
+	else:
+		print("Erreur : Impossible de charger la scène.")
