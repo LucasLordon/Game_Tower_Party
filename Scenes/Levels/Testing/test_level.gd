91d8ee21e4e83c5c091f2f2ebd38fc655a15ef8b
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2023-11-17 17:55:15 +0100

    edit dropper

--- Scenes/Levels/Testing/test_level.gd
+++ Scenes/Levels/Testing/test_level.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
-func _on_player_dropper_piece_detected(chosePiece: String):
+func _on_player_dropper_piece_detected(chosePiece):
 	print(chosePiece)
-	var piece_scene = load(chosePiece)   
-	if piece_scene and piece_scene is PackedScene:
-		var piece_instance = piece_scene.instance()      
-		if piece_instance and piece_instance is RigidBody2D:
-			var piece = piece_instance as RigidBody2D
-			piece.position = $PlayerDropper.global_position
-			$Pieces.add_child(piece)
-		else:
-			print("Erreur : La scène chargée n'est pas une instance de RigidBody2D.")
-	else:
-		print("Erreur : Impossible de charger la scène.")
+#	if chosePiece:
+	print(chosePiece)
+	var piece_instance = preload(chosePiece).instantiate() as RigidBody2D
+	piece_instance.position = $PlayerDropper.global_position
+	$Pieces.add_child(piece_instance)
+#		if piece_instance and piece_instance is RigidBody2D:
+#			print(chosePiece)
+#			var piece = piece_instance as RigidBody2D
+#			piece.position = $PlayerDropper.global_position
+#			$Pieces.add_child(piece)
+#		else:
+#			print("Erreur : La scène chargée n'est pas une instance de RigidBody2D.")
+#	else:
+#		print("Erreur : Impossible de charger la scène.")
