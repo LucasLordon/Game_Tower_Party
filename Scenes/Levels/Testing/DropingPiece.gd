44e4f9788345dcb4c8c4cb17412e577e9b807bcc
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2023-12-04 13:45:17 +0100

    Make colisition between dropping pieces and other thing working

--- Scenes/Levels/Testing/DropingPiece.gd
+++ Scenes/Levels/Testing/DropingPiece.gd
@@ -8,12 +8,24 @@ func _process(delta):
 		if child is RigidBody2D:
 			# Récupère le détecteur de collisions (Area2D ou CollisionShape2D)
 			var collision_detector = child.get_child(0)  # Assurez-vous d'ajuster l'index selon votre scène
+<<<<<<< Updated upstream
 			piece_instance = child
 			# Vérifie si le détecteur de collisions a des collisions
 			if collision_detector.get_overlapping_areas().size() > 0:
 				# Il y a une collision avec ce RigidBody2D, faites quelque chose ici
 				IsTouchingSomeThingElse = true
 	if IsTouchingSomeThingElse == true:
+=======
+			# Vérifie si le détecteur de collisions a des collisions
+			if collision_detector.get_overlapping_areas ().size() > 0:
+				# Il y a une collision avec ce RigidBody2D, faites quelque chose ici
+				IsTouchingSomeThingElse = true
+	if IsTouchingSomeThingElse == true:
+		var pieces_node = get_node("../Pieces")
+		for child2 in get_children():
+			remove_child(child2)
+			pieces_node.add_child(child2)
+>>>>>>> Stashed changes
 		colision.emit(true)
 		$"../Pieces".add_child(piece_instance)
 
