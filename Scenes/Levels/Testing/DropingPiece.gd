569c2a4e3d4ec30ace0222d9e2289160dcab762d
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2023-12-04 13:56:46 +0100

    Make a new branch and the dp_piece work

--- Scenes/Levels/Testing/DropingPiece.gd
+++ Scenes/Levels/Testing/DropingPiece.gd
@@ -5,19 +5,17 @@ signal colision(bool)
 func _process(delta):
 	# Vérifie si l'un des enfants a des collisions
 	for child in get_children():
-		print("Body")
 		if child is RigidBody2D:
 			# Récupère le détecteur de collisions (Area2D ou CollisionShape2D)
 			var collision_detector = child.get_child(0)  # Assurez-vous d'ajuster l'index selon votre scène
-			
-			print("rigidBody")
 			# Vérifie si le détecteur de collisions a des collisions
-			if collision_detector.get_overlapping_areas().size() > 0:
-				print("touching0")
+			if collision_detector.get_overlapping_areas ().size() > 0:
 				# Il y a une collision avec ce RigidBody2D, faites quelque chose ici
 				IsTouchingSomeThingElse = true
 	if IsTouchingSomeThingElse == true:
+		var pieces_node = get_node("../Pieces")
 		for child2 in get_children():
-			$"../Pieces".add_child(child2.get_child(0))
+			remove_child(child2)
+			pieces_node.add_child(child2)
 		colision.emit(true)
 		IsTouchingSomeThingElse = false
